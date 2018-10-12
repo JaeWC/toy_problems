44384fe479e1abbc7f23157382735aec45653f5a
Jae Choi <j.thechois@gmail.com>
2018-10-12 09:59:15 +0900

    complete toy10

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -6,11 +6,34 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  var createSpan = () => {
+    $('p').each((idx, elem) => {
+      let splitted =  $(elem).html().split(' ')
+      let array = splitted.map(elem => '<span>' + elem + '</span>')
 
+      $('p').html(array.join(' '))
+    })
+  }
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
+  // console.log($('span'))
 
+  var changeColor = () => {
+
+    $('span').each((idx, elem) => {
+      const r = Math.floor( Math.random() * 255 );
+      const g = Math.floor( Math.random() * 255 );
+      const b = Math.floor( Math.random() * 255 );
+      const rgb = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+
+      $(elem).css('color', rgb );
+    });
+  };
+
+  createSpan();
+
+  setInterval(changeColor, 1000)
 });
