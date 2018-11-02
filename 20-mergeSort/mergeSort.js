c08626c17161ee708ca026fdf7077f2c7d79db90
Jae Choi <j.thechois@gmail.com>
2018-11-02 09:32:00 +0900

    complete toy20

--- 20-mergeSort/mergeSort.js
+++ 20-mergeSort/mergeSort.js
@@ -95,8 +95,33 @@
  *
  */
 
+merge = (left, right) => {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
 
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  console.log(
+    result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
+  );
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
 
 var mergeSort = function(array) {
   // Your code here.
+  if (array.length === 1) return array;
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
 };
