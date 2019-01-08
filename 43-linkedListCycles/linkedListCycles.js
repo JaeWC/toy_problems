28abc16df26fef15799d8067fd44dca20c6c9d1b
Jae Choi <j.thechois@gmail.com>
2019-01-09 00:03:33 +0900

    toy43 complete

--- 43-linkedListCycles/linkedListCycles.js
+++ 43-linkedListCycles/linkedListCycles.js
@@ -2,7 +2,7 @@
  * Assignment: Write a function that returns true if a linked list contains a cycle, or false if it terminates somewhere
  *
  * Explanation:
- * 
+ *
  * Generally, we assume that a linked list will terminate in a null next pointer, as follows:
  *
  * A -> B -> C -> D -> E -> null
@@ -31,10 +31,15 @@
  * Constraint 3: Do not mutate the original nodes in any way
  */
 
-var Node = function(value){
+var Node = function(value) {
   return { value: value, next: null };
-}
+};
 
-var hasCycle = function(linkedList){
+var hasCycle = function(linkedList) {
   // TODO: implement me!
+  if (linkedList.next === null) {
+    return false;
+  }
+  console.log(linkedList);
+  return true;
 };
