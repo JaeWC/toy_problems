ed3fad835fb1455464b2e109822534d9d2f6a278
Jae Choi <j.thechois@gmail.com>
2019-01-09 18:39:00 +0900

    toy44 Complete

--- 44-sortableTable/sortableTable.js
+++ 44-sortableTable/sortableTable.js
@@ -8,8 +8,28 @@
  * Use the table provided in index.html.
  **/
 
-$(function () {
+$(function() {
   // TODO: your code here!
+  var $tbody = $('tbody');
+  var $rows = $tbody.children();
 
+  var getType = function(colIndex) {
+    var val = $($rows[0].children[colIndex]).text();
+
+    return isNaN(val) ? 'string' : 'number';
+  };
+
+  $('th').on('click', function(e) {
+    var colIndex = $(this).index();
+    var type = getType(colIndex);
+    $rows = $rows.detach().sort(function(row1, row2) {
+      var value1 = $(row1.children[colIndex]).text();
+      var value2 = $(row2.children[colIndex]).text();
+
+      if (type === 'string') return value1 > value2;
+      if (type === 'number') return value1 - value2;
+    });
+  });
+
+  $tbody.append($rows);
 });
-
