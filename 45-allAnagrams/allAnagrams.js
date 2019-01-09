24ba80eb251bf705db97a797e9ea637a2f79e5d0
Jae Choi <j.thechois@gmail.com>
2019-01-10 01:12:18 +0900

    toy45 complete

--- 45-allAnagrams/allAnagrams.js
+++ 45-allAnagrams/allAnagrams.js
@@ -7,11 +7,28 @@
  */
 
 /**
-  * example usage:
-  * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example usage:
+ * var anagrams = allAnagrams('abc');
+ * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
 var allAnagrams = function(string) {
   // Your code here.
+  if (string.length === 1) return [string];
+
+  var uniqOutput = {};
+
+  function recursion(ana, string) {
+    if (string === '') {
+      uniqOutput[ana] = 1;
+    }
+
+    for (var i = 0; i < string.length; i++) {
+      recursion(ana + string[i], string.slice(0, i) + string.slice(i + 1));
+    }
+  }
+
+  recursion('', string);
+
+  return Object.keys(uniqOutput);
 };
